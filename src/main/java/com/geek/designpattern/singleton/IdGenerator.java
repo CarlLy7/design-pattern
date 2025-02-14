3cff24e15d966ed6347021d8a0f60c02e5bb45f3
luanyingqi <yingqi.luan@skp-beijing.com>
2025-02-14 17:57:06 +0800

    单例模式

---
+++ src/main/java/com/geek/designpattern/singleton/IdGenerator.java
@@ -0,0 +1,26 @@
+package com.geek.designpattern.singleton;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 使用静态内部类实现单例模式
+ *
+ * @author: carl
+ * @date: 2025.02.14
+ */
+
+public class IdGenerator {
+    private AtomicLong id = new AtomicLong(0);
+
+    private static class SingletonHodler {
+        private static final IdGenerator instance = new IdGenerator();
+    }
+
+    public static IdGenerator getInstance() {
+        return SingletonHodler.instance;
+    }
+
+    public long getId() {
+        return id.incrementAndGet();
+    }
+}
---
+++ src/main/java/com/geek/designpattern/singleton/IdGenerator2.java
@@ -0,0 +1,18 @@
+package com.geek.designpattern.singleton;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 使用枚举实现单例模式
+ *
+ * @author: carl
+ * @date: 2025.02.14
+ */
+public enum IdGenerator2 {
+    INSTANCE;
+    private AtomicLong id = new AtomicLong(0);
+
+    public long getId() {
+        return id.incrementAndGet();
+    }
+}
